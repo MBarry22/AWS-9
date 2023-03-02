edbbb5d121c91fddc10383c7fe5ea7ac3f3d2d05
Mason Barry <mbarry22@my.bcit.ca>
2023-03-02 09:16:02 -0800

    Working Weather and Poke

---
+++ backend/index.js
@@ -0,0 +1,114 @@
+import pg from 'pg'
+const { Pool } = pg
+
+let pool
+
+const getLocations = async (event) => {
+  const res = await pool.query(`
+    select locations.name, locations.timezone, weather.temperature, weather.description, weather.user_uuid
+    from locations
+    join weather on locations.id = weather.location_id;
+  `)
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(res.rows),
+  };
+  return response;
+}
+
+const createLocation = async (event) => {
+  const newLocation = JSON.parse(event.body);
+  console.log(newLocation)
+
+  // Get the currently logged in user
+  const userId = event.requestContext.authorizer.jwt.claims.sub
+
+  // Persist my locations object
+  await pool.query('BEGIN');
+
+  // Insert the new location data
+  const locationQuery = {
+    text: 'INSERT INTO locations (name, timezone, user_uuid) VALUES ($1, $2, $3) RETURNING id',
+    values: [newLocation.name, newLocation.timezone, userId]
+  };
+  const locationResult = await pool.query(locationQuery);
+
+  // Insert the new weather data
+  const weatherQuery = {
+    text: 'INSERT INTO weather (location_id, timestamp, temperature, description, user_uuid) VALUES ($1, $2, $3, $4, $5)',
+    values: [locationResult.rows[0].id, new Date(), newLocation.weather.temperature, newLocation.weather.description, userId]
+  };
+  await pool.query(weatherQuery);
+
+  // Commit the transaction
+  await pool.query('COMMIT');
+
+  const response = {
+    statusCode: 201,
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({message: "ok"}),
+  };
+  return response;
+}
+
+const deleteLocation = async (event) => {
+  const locationId = event.pathParameters.id;
+  const userId = event.requestContext.authorizer.jwt.claims.sub;
+
+  await pool.query('BEGIN');
+
+  // Delete the location and its associated weather data
+  const deleteWeatherQuery = {
+    text: 'DELETE FROM weather WHERE location_id = $1 AND user_uuid = $2',
+    values: [locationId, userId]
+  };
+  await pool.query(deleteWeatherQuery);
+
+  const deleteLocationQuery = {
+    text: 'DELETE FROM locations WHERE id = $1 AND user_uuid = $2',
+    values: [locationId, userId]
+  };
+  await pool.query(deleteLocationQuery);
+
+  // Commit the transaction
+  await pool.query('COMMIT');
+
+  const response = {
+    statusCode: 204,
+    body: ''
+  };
+  return response;
+}
+
+export const handler = async (event) => {
+  if (!pool) {
+    const connectionString = process.env.DATABASE_URL;
+    pool = new Pool({
+      connectionString,
+      application_name: "",
+      max: 1,
+    });
+  }
+
+  console.log(event.requestContext.http.method)
+  switch (event.requestContext.http.method) {
+    case 'GET':
+      return await getLocations(event);
+    case 'POST':
+        return await createLocation(event);
+    case 'DELETE':
+        return await deleteLocation(event);
+    default:
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed'
+        }
+    }
+}
+
--- pokemon/index.js
+++ pokemon/index.js
@@ -2,121 +2,98 @@ import pg from 'pg'
 const { Pool } = pg
 
 let pool
+
 const getPokemon = async (event) => {
   const res = await pool.query(`
-    SELECT * FROM pokemon;
-  `)
+  select * from pokemon;
+`)
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'content-type': 'application/json'
-
-    },
-    body: JSON.stringify(res.rows),
-  };
-  return response;
+const response = {
+statusCode: 200,
+headers: {
+'content-type': 'application/json'
+},
+body: JSON.stringify(res.rows),
+};
+return response;
 }
 
 const createPokemon = async (event) => {
-  const newPokemon = JSON.parse(event.body);
-  console.log(newPokemon)
+const newPokemon = JSON.parse(event.body);
+console.log(newPokemon)
 
-  // Persist the new pokemon data
-  await pool.query('BEGIN');
+// Get the currently logged in user
+const userId = event.requestContext.authorizer.jwt.claims.sub
 
-  const pokemonQuery = {
-    text: 'INSERT INTO pokemon (name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id',
-    values: [newPokemon.name, newPokemon.type1, newPokemon.type2, newPokemon.hp, newPokemon.attack, newPokemon.defense, newPokemon.sp_attack, newPokemon.sp_defense, newPokemon.speed, newPokemon.total]
-  };
-  const pokemonResult = await pool.query(pokemonQuery);
+// Persist my pokemon object
+await pool.query('BEGIN');
 
-  // Commit the transaction
-  await pool.query('COMMIT');
+// Insert the new pokemon data
+const pokemonQuery = {
+text: 'INSERT INTO pokemon (user_uuid, name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id',
+values: [userId, newPokemon.name, newPokemon.type1, newPokemon.type2, newPokemon.hp, newPokemon.attack, newPokemon.defense, newPokemon.sp_attack, newPokemon.sp_defense, newPokemon.speed, newPokemon.total]
+};
+const pokemonResult = await pool.query(pokemonQuery);
 
-  const response = {
-    statusCode: 201,
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({id: pokemonResult.rows[0].id}),
-  };
-  return response;
-}
+// Commit the transaction
+await pool.query('COMMIT');
 
-const updatePokemon = async (event) => {
-  const pokemonId = event.pathParameters.id;
-  const updatedPokemon = JSON.parse(event.body);
-  console.log(updatedPokemon);
-
-  await pool.query('BEGIN');
-
-  // Update the pokemon data
-  const updateQuery = {
-    text: 'UPDATE pokemon SET name = $1, type1 = $2, type2 = $3, hp = $4, attack = $5, defense = $6, sp_attack = $7, sp_defense = $8, speed = $9, total = $10 WHERE id = $11',
-    values: [updatedPokemon.name, updatedPokemon.type1, updatedPokemon.type2, updatedPokemon.hp, updatedPokemon.attack, updatedPokemon.defense, updatedPokemon.sp_attack, updatedPokemon.sp_defense, updatedPokemon.speed, updatedPokemon.total, pokemonId]
-  };
-  await pool.query(updateQuery);
-
-  // Commit the transaction
-  await pool.query('COMMIT');
-
-  const response = {
-    statusCode: 200,
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({message: "ok"}),
-  };
-  return response;
+const response = {
+statusCode: 201,
+headers: {
+'content-type': 'application/json'
+},
+body: JSON.stringify({message: "ok"}),
+};
+return response;
 }
 
 const deletePokemon = async (event) => {
-  const pokemonId = event.pathParameters.id;
+  console.log(event)
+const pokemonId = event.pathParameters.id;
+console.log(event)
 
-  await pool.query('BEGIN');
+await pool.query('BEGIN');
 
-  // Delete the pokemon data
-  const deleteQuery = {
-    text: 'DELETE FROM pokemon WHERE id = $1',
-    values: [pokemonId]
-  };
-  await pool.query(deleteQuery);
+// Delete the pokemon
+const deletePokemonQuery = {
+text: 'DELETE FROM pokemon WHERE id = $1',
+values: [pokemonId]
+};
+await pool.query(deletePokemonQuery);
 
-  // Commit the transaction
-  await pool.query('COMMIT');
+// Commit the transaction
+await pool.query('COMMIT');
 
-  const response = {
-    statusCode: 204,
-    body: ''
-  };
-  return response;
+const response = {
+statusCode: 204,
+body: ''
+};
+return response;
 }
 
 export const handler = async (event) => {
-  if (!pool) {
-    const connectionString = process.env.DATABASE_URL;
-    pool = new Pool({
-      connectionString,
-      application_name: "",
-      max: 1,
-    });
-  }
-
-  console.log(event.requestContext.http.method)
-  switch (event.requestContext.http.method) {
-    case 'GET':
-        return getPokemon(event);
-    case 'POST':
-        return createPokemon(event);
-    case 'DELETE':
-        return deletePokemon(event);
-    default:
-        return {
-            statusCode: 405,
-            body: 'Method Not Allowed'
-        };
-    }
+if (!pool) {
+const connectionString = process.env.DATABASE_URL;
+pool = new Pool({
+connectionString,
+application_name: "",
+max: 1,
+});
 }
 
-// Path: backend\pokemons.js
+console.log(event.requestContext.http.method)
+switch (event.requestContext.http.method) {
+case 'GET':
+return await getPokemon(event);
+case 'POST':
+return await createPokemon(event);
+case 'DELETE':
+return await deletePokemon(event);
+default:
+return {
+statusCode: 405,
+body: 'Method Not Allowed'
+}
+}
+}
